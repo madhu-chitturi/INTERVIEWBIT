333191af7aa85b79d2690940b72dea2ca6bc3428
CHITTURI MADHU SEETARAM <106515693+madhu-chitturi@users.noreply.github.com>
2022-10-22 11:10:47 +0530

    Pair With Given Difference.py

---
+++ Pair With Given Difference.py
@@ -0,0 +1,32 @@
+link:  https://www.interviewbit.com/problems/pair-with-given-difference/
+
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        dict1={}
+        if(B==0):
+            c=set(A)
+            if(len(c)!=len(A)):
+                return 1
+            else:
+                return 0
+        for i in range(len(A)):
+            if A[i] in dict1:
+                dict1[A[i]]+=1
+            else:
+                dict1[A[i]]=1
+        for i in range(len(A)):
+            a=A[i]
+            b=a-B 
+            if b in dict1:
+                c=1
+                break
+            else:
+                c=0
+        if(c==1):
+            return 1
+        else:
+            return 0
+                
