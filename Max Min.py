1e61b026e3a3c3a3681f86e6ec3b9df50b73e621
CHITTURI MADHU SEETARAM <106515693+madhu-chitturi@users.noreply.github.com>
2022-10-23 15:30:14 +0530

    Max Min.py

---
+++ Max Min.py
@@ -0,0 +1,11 @@
+link: https://www.interviewbit.com/problems/max-min-05542f2f-69aa-4253-9cc7-84eb7bf739c4/
+
+
+
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def solve(self, A):
+        max1=max(A)
+        min1=min(A)
+        return max1+min1
